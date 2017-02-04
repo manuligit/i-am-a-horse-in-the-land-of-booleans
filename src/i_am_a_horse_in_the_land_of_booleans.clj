37675c4caded6774c894d183643539c15ab8e494
manuligit <riina.vaisanen@gmail.com>
2017-02-04 23:42:52 +0200

    exer 4-8

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -16,18 +16,34 @@
      false))
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond (= (mod n 15) 0) "gotcha!"
+        (= (mod n 3) 0) "fizz"
+        (= (mod n 5) 0) "buzz"
+        :else            "")
+  )
 
 (defn teen? [age]
   (<= 13 age 19))
 
 (defn not-teen? [age]
-  ":(")
+  (not (teen? age)))
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond (number? x) (* x 2)
+        (empty? x) nil
+        (list? x) (* (count x) 2)
+        (vector? x) (* (count x) 2)
+  :else true
+  ))
+
+
 
 (defn leap-year? [year]
-  ":(")
+  (cond (= (mod year 400) 0) true
+        (= (mod year 100) 0) false
+        (= (mod year 4) 0) true
+        :else false)
+  )
+
 
 ; '_______'
